1bc311df5798f2b6bc648023015529289b7e9bba
Inam Ahmad-zada <inam.ahmadzada@gmail.com>
2016-11-30 17:34:00 -0800

    Finished user interface: added requred UIViews

--- Scribe/ViewController.swift
+++ Scribe/ViewController.swift
@@ -10,16 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var activitySpinner: UIActivityIndicatorView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func playBtnPressed(_ sender: UIButton) {
     }
-
 
 }
 
